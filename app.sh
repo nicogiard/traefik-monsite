201adc8ed64eaf69df0ab731d4c93dbe787edeed
Nicolas Giard <nicogiard@gmail.com>
2018-11-07 14:25:02 +0100

    correction d'une typo dans app.sh

--- app.sh
+++ app.sh
@@ -6,7 +6,7 @@ docker run --name monsite -v /home/nicogiard/tmp/traefik/monsite:/app -p 9000:90
  --label "traefik.phhome.frontend.rule=Host:www.monsite.ph" \
  --label "traefik.phhome.port=9000" \
  --label "traefik.phreste.frontend.redirect.regex=^http://www.monsite.ph/(.*)" \
- --label "traefik.phreste.frontend.redirect.replacement=http://www.monsite.fr/$${1}" \
+ --label "traefik.phreste.frontend.redirect.replacement=http://www.monsite.fr/$1" \
  --label "traefik.phreste.frontend.rule=Host:www.monsite.ph" \
  --label "traefik.phreste.port=9000" \
  --label "traefik.www.frontend.rule=Host:www.monsite.fr" \
